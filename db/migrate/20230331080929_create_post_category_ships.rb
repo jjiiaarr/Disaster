762cb2ece35f56fa8e24ae9a2b42239230c82a72
jjiiaarr <jiiarr.glorioso@gmail.com>
2023-03-31 16:11:37 +0800

    migrate categories and categories ship

--- db/migrate/20230331080929_create_post_category_ships.rb
+++ db/migrate/20230331080929_create_post_category_ships.rb
@@ -1,7 +1,8 @@
 class CreatePostCategoryShips < ActiveRecord::Migration[7.0]
   def change
     create_table :post_category_ships do |t|
-
+      t.references :post
+      t.references :category
       t.timestamps
     end
   end
