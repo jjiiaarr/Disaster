7086c01fe724bb1a67dc3820e416ba9c2ee82738
jjiiaarr <jiiarr.glorioso@gmail.com>
2023-04-10 14:02:28 +0800

    config routes for users path own posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/index'
   devise_for :users
 
   get 'welcome' => 'welcome#index'
   root 'posts#index'
+  get 'users/:id', to: 'users#index', as: 'user'
 
   resources :posts do
     resources :comments, except: :show
